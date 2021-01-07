03240db81cf0f17a5b16d4c0c98c5be76413d241
GabrielSSouza97 <gabriel.s.souza9755@gmail.com>
2021-01-07 16:08:44 -0300

    Extraindo os nomes
    Código de teste para extrair os nomes dos filmes e séries, confirmando que temos acesso à api

--- beca-filmes/beca-filmes/Home/Model/FilmesAPI.swift
+++ beca-filmes/beca-filmes/Home/Model/FilmesAPI.swift
@@ -17,7 +17,28 @@ class FilmesAPI: NSObject {
         Alamofire.request("https://api.themoviedb.org/3/trending/all/week?api_key=e8606b85a11ce3fb1de18d901be3f16c&language=pt-BR", method: .get).responseJSON { (response) in
             switch response.result {
             case .success:
-                print(response.result.value!)
+                if let resposta = response.result.value as? Dictionary<String, Any> {
+                    guard let listaDeTendencias = resposta["results"] as? Array<Dictionary<String, Any>> else { return }
+                    for dicionarioDeFilmes in listaDeTendencias {
+                        guard let tipo = dicionarioDeFilmes["media_type"] as? String else {
+                            print("Deu errado :(")
+                            return
+                        }
+                        if tipo == "movie" {
+                            guard let titulo = dicionarioDeFilmes["title"] as? String else {
+                                print("Deu errado no titulo :(")
+                                return
+                            }
+                            print(titulo)
+                        } else {
+                            guard let nome = dicionarioDeFilmes["name"] as? String else {
+                                print("Deu errado no título :(")
+                                return
+                            }
+                            print(nome)
+                        }
+                    }
+                }
                 break
             case .failure:
                 print(response.error!)
