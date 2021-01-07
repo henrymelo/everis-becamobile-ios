62b18fefa0dcb1cc97729c51a48674e323599662
Fabricio Rodrigo Baixo <fabriciosenna@gmail.com>
2021-01-07 11:01:55 -0300

    Criação do projeto cinesofa para a avaliação Beca Mobile IOS

---
+++ cinesofa/cinesofa/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  cinesofa
+//
+//  Created by Fabricio Rodrigo Baixo on 1/7/21.
+//  Copyright © 2021 Fabricio Rodrigo Baixo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+
+
+}
+
