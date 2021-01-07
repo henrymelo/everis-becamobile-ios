0c2598a6fd9a9b3116b6d3b10219484a57987253
Fabricio Rodrigo Baixo <fabriciosenna@gmail.com>
2021-01-07 12:10:07 -0300

    Testando retorno das informações de filmes  usando a API do site:: developers.themoviedb.org

--- cinesofa/cinesofa/ViewController.swift
+++ cinesofa/cinesofa/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print(FilmesAPI().maisVistosDaSemana())
+        print(FilmesAPI().informacoesFilme())
     }
     
     
