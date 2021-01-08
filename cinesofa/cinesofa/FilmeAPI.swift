3eb5b2017e991ec00481e9cc168664f42a65f158
Fabricio Rodrigo Baixo <fabriciosenna@gmail.com>
2021-01-08 04:49:20 -0300

    Recuperando informações da API e armazenando no Coredata

---
+++ cinesofa/cinesofa/FilmeAPI.swift
@@ -0,0 +1,15 @@
+//
+//  FilmeAPI.swift
+//  cinesofa
+//
+//  Created by Fabricio Rodrigo Baixo on 1/7/21.
+//  Copyright © 2021 Fabricio Rodrigo Baixo. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+class FilmeAPI{
+    
+}
--- cinesofa/cinesofa/ViewController.swift
+++ cinesofa/cinesofa/ViewController.swift
@@ -8,18 +8,18 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        print(FilmesAPI().maisVistosDaSemana())
-        print(FilmesAPI().informacoesFilme())
+        
     }
     
-    
+   
 
+   
 
 }
 
