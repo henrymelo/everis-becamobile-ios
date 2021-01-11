9d3447320c6e5a375c215d8753a7ca9e8c8f1b80
Letycia <letycia.sobrinho@hotmail.com>
2021-01-10 23:02:37 -0300

    escoposegundatela

--- Filmes/Filmes/Controllers/DetalhesDoFilmeViewController.swift
+++ Filmes/Filmes/Controllers/DetalhesDoFilmeViewController.swift
@@ -8,24 +8,31 @@
 import UIKit
 
 class DetalhesDoFilmeViewController: UIViewController {
-
+    
+    // MARK: IBOutlets - detalhes filmes
+    
+    @IBOutlet weak var tituloFilme: UILabel!
+        
+    @IBOutlet weak var imagemDoFilme: UIImageView!
+    
+    @IBOutlet weak var textoSinopse: UILabel!
+    
+    @IBOutlet weak var textoLancamento: UILabel!
+    
+    
+    // MARK: IBAction
+    
+    
+    @IBAction func voltar(_ sender: UIButton) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+       
     var meusFilmes:[String:Any]? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Filmes/Filmes/Models/DetalhesFilmeAPI.swift
+++ Filmes/Filmes/Models/DetalhesFilmeAPI.swift
@@ -11,6 +11,13 @@ import Alamofire
 
 class DetalhesFilmeAPI: NSObject {
     
+    
+  
+    
+    
+    
+    // MARK: Variaveis
+    
     var listaFilmes:Array<String> = ["Mulher Maravilha"]
     let minhaChave:String = "fda3fa048b5dd1721be8d5512626b16a"
     var urlDetalhesFilmes:String = "https://api.themoviedb.org/3/movie/454626?api_key=fda3fa048b5dd1721be8d5512626b16a&language=pt-BR"
