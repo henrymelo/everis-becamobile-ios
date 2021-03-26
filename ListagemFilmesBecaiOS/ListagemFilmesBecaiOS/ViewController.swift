0edb91145fc0b51d7aa51a6d36ec74b4bd25127f
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2021-03-26 11:59:35 -0300

    :tada: Adicionado projeto xcode com tela inicial padrao

---
+++ ListagemFilmesBecaiOS/ListagemFilmesBecaiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListagemFilmesBecaiOS
+//
+//  Created by Lucas Werner Kuipers on 26/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
